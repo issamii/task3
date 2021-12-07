eda51b4abf88f529e88454457d1ea3f64ced2e9e
issamii <asmiadhikari09@gmail.com>
2021-12-07 16:47:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-psugxk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-psugxk)
---
+++ src/MedicalTreatmentPanel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function MedicalTreatmentPanel () {
+  const [treatID, settreatID] = React.useState('');
+
+  const [treatCourseId, settreatCourseId] = React.useState('');
+
+  const [productDatePurchased, setProductDatePurchased] = React.useState('');
+}
